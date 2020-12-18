8c1117747b5c83da8993fdb7a95b73178fb97135
FeiTai <745502274@qq.com>
2020-12-18 13:59:00 +0800

    add style

--- AutoPicture.js
+++ AutoPicture.js
@@ -16,6 +16,11 @@ class AutoPicture {
     static frag = document.createDocumentFragment();
 
     create() {
+        const ele_style = {
+            dis: 'block',
+            mg: 0,
+            pd: 0,
+        }
         for (let i = this.defaultOptions.start; i < this.defaultOptions.end; i++) {
             //创建image对象
             const img = document.createElement('img');
@@ -24,12 +29,12 @@ class AutoPicture {
             //设定路径与类型
             img.src = this.defaultOptions.src + i + this.defaultOptions.type;
             //防止图片间隔出现白线
-            img.style.display = 'block';
+            img.style.display = ele_style.dis;
             img.className = this.defaultOptions.name;
             //设定容器data
             div.className = 'items';
-            div.style.margin = '0';
-            div.style.padding = '0';
+            div.style.margin = ele_style.mg;
+            div.style.padding = ele_style.pd;
             //容器添加img
             div.appendChild(img);
             AutoPicture.frag.appendChild(div);
